ad65e33f3c7e1c58422d578004460cdeaa1ae9d7
Mekel Tensley <mekelintech@gmail.com>
2021-11-10 17:18:45 -0500

    Complete function that allows x and o to appear on grid

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // current players turn 
-let currentPlayer = "X";
+let currentPlayer = "x";
 
-const tiles = document.getElementsByClassName('tic'); 
+const tiles = document.querySelectorAll('.tic'); 
 
 // this loops through all of the tiles on the grid
 for (let i = 0; i < tiles.length; i++) {
@@ -11,12 +11,68 @@ for (let i = 0; i < tiles.length; i++) {
         //checks if the tile has an X or O
         if (tiles[i].innerHTML == "") {
             //adds X or O for the current play in their choosen tile 
-            tiles[i].innerHTML.trim() = currentPlayer;
+            tiles[i].innerHTML = currentPlayer;
 
-            currentPlayer = currentPlayer == "X" ? "O" : "X";
+            currentPlayer = currentPlayer == "x" ? "o" : "x";
 
-            document.getElementById('')
+            document.getElementById('player').innerHTML = currentPlayer.toUpperCase();
+            
+
+            //checks 3 matching x's or o's
+
+            if (
+                tiles[0].innerHTML == tiles[1].innerHTML &&
+                tiles[1].innerHTML == tiles[2].innerHTML &&
+                tiles[0].innerHTML.trim() != ""
+              ) {
+
+              } else if (
+                tiles[3].innerHTML == tiles[4].innerHTML &&
+                tiles[4].innerHTML == tiles[5].innerHTML &&
+                tiles[3].innerHTML.trim() != ""
+              ) {
+                
+              } else if (
+                tiles[6].innerHTML == tiles[7].innerHTML &&
+                tiles[7].innerHTML == tiles[8].innerHTML &&
+                tiles[6].innerHTML.trim() != ""
+              ) {
+                
+              } else if (
+                tiles[0].innerHTML == tiles[3].innerHTML &&
+                tiles[3].innerHTML == tiles[6].innerHTML &&
+                tiles[0].innerHTML.trim() != ""
+              ) {
+                
+              } else if (
+                tiles[1].innerHTML == tiles[4].innerHTML &&
+                tiles[4].innerHTML == tiles[7].innerHTML &&
+                tiles[1].innerHTML.trim() != ""
+              ) {
+                
+              } else if (
+                tiles[2].innerHTML == tiles[5].innerHTML &&
+                tiles[5].innerHTML == tiles[8].innerHTML &&
+                tiles[2].innerHTML.trim() != ""
+              ) {
+                
+              } else if (
+                tiles[0].innerHTML == tiles[4].innerHTML &&
+                tiles[4].innerHTML == tiles[8].innerHTML &&
+                tiles[0].innerHTML.trim() != ""
+              ) {
+            
+              } else if (
+                tiles[2].innerHTML == tiles[4].innerHTML &&
+                tiles[4].innerHTML == tiles[6].innerHTML &&
+                tiles[2].innerHTML.trim() != ""
+              ) {
+          
+            }
+            
         }
 
     });
 }
+
+function displayWinner ()
